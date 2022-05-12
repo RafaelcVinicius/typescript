0a022907ba8875b10075017d1608dfd92fbbd46b
RafaelcVinicius <vinicius.colde@gmail.com>
2022-05-11 23:00:03 -0300

    cpf

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+     <!-- <div> -->
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    <!-- </div> -->
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&display=swap');
+* {
+  margin: 0;
+  border: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  letter-spacing: 1px;
+  color: #000408;
+  background-color: #fff;
 }
-
+a {
+  color: #cccccc;
+  text-decoration: none;
+}
 nav {
-  padding: 30px;
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  background-color: #000408;
 }
-
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  width: 100px;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.home{
+margin: 30px;
 }
+.div-titulo{
+  display: flex;
+  justify-content: flex-start;
+  max-width:1000px;
+  width: 100%;
+}
+.titulo{
+  font-size: 25px;
+}
+.div-acoes{
+  display: flex;
+  justify-content: space-between;
+  max-width:550px;
+  width: 100%;
+}
+.div-btn {
+  max-width: 200px;
+  width: 100%;
+  height: 40px;
+  border: none;
+  background-color: #1f4c72;
+  color: #cccccc;
+  border-radius: 8px;
+  font-size: 16px;
+  box-shadow: 3px 3px 10px #cccccc;
+  letter-spacing: 2px;
+  cursor: pointer;
+}
+.div-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 550px;
+  width: 100%;
+  height: 150px;
+  background-color: rgb(241, 241, 241);
+  border: 1px solid #cccccc;
+  font-size: 35px;
+  }
 </style>
--- src/components/GeradorCpf.vue
+++ src/components/GeradorCpf.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="cpf">
-    <h4 class="titulo">Gerador de CNPJ</h4>
-    <div class="div-btn">
-      <button @click="gerarCPF">Gerar</button>
+    <div class="div-titulo">
+      <h4 class="titulo">Gerador de CNPJ</h4>
+    </div>
+    <div class="div-acoes">
+      <span></span>
+      <button class="div-btn" @click="gerarCPF">Gerar</button>
     </div>
     <div class="div-content">
       <span>{{ cpf }}</span>
@@ -12,13 +15,10 @@
 
 <script lang="ts">
 
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 
 export default defineComponent({
   name: 'GeradorCpf',
-  setup() {
-    // const num = ref<number | string>(0)
-  },
   data() {
     return {
       cpf: '' as string | number,
@@ -40,7 +40,7 @@ export default defineComponent({
       this.numdvpri = String(this.dvpri(this.dig1, this.dig2, this.dig3));
       this.numdvseg = String(this.dvseg(this.dig1, this.dig2, this.dig3, this.numdvpri));
 
-      this.cpf = this.dig1 + this.dig2 + this.dig3 + this.numdvpri + this.numdvseg;
+      this.cpf = String(this.dig1 + this.dig2 + this.dig3 + this.numdvpri + this.numdvseg);
     },
 
     numeroRandom() {
@@ -95,46 +95,12 @@ export default defineComponent({
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&display=swap');
-  body{
-    font-family: 'Poppins', sans-serif;
-    letter-spacing: 1px;
-  }
-  #cpf{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 2rem;
-    color: rgb(77, 76, 76);
-  }
-  .titulo{
-    font-size: 25px;
-  }
-  .div-btn{
-    max-width: 200px;
-    width: 100%;
-  }
-  .div-btn button{
-    width: 100%;
-    height: 40px;
-    border: none;
-    background-color: rgb(122, 108, 190);
-    color: #fff;
-    border-radius: 8px;
-    font-size: 16px;
-    box-shadow: 3px 3px 10px rgb(156, 156, 156);
-    letter-spacing: 2px;
-  }
-  .div-content{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 450px;
-    width: 100%;
-    height: 150px;
-    background-color: rgb(241, 241, 241);
-    border: 1px solid rgb(145, 145, 145);
-    font-size: 35px;
-  }
+#cpf{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  color: rgb(77, 76, 76);
+}
 </style>
