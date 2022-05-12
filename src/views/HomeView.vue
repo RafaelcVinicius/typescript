3841d1be0197cd9a27253d88675bf6dd83d7497a
RafaelcVinicius <vinicius.colde@gmail.com>
2022-05-11 21:15:15 -0300

    gerador de cpf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import GeradorCpf from '@/components/possoUm.vue';
+import GeradorCpf from '@/components/GeradorCpf.vue';
 
 export default defineComponent({
   name: 'HomeView',
